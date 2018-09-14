cbc4882046ba60d5664c676b25ea039bc837abde
33marvil <martinramirezcruz71@gmail.com>
2018-09-14 13:51:18 -0500

    Athlete-Class-in-Python-version-2

--- athlete/athlete.py
+++ athlete/athlete.py
@@ -28,33 +28,79 @@ class Athlete:
     def speed(self, speed):
         self.__speed = speed
 
+    # 2da parte
+    @total_time.setter
+    def total_time(self, total_time):
+        if self.__total_time == 0 and self.__total_time == total_time:
+            raise Exception
 
+    def speed_record(self):
+        # si velocidad = 0 retur 0 y speed es = 0
+        # si velocidad es > 0 return velocidad y actauliza speed, que es resultado de distacia / tiempo
+        if self.__total_time == 0:
+            self.__speed = 0
+            return self.__speed
+        else:
+            self.__speed = (self.__total_distance / self.__total_time)
+            return round(self.__speed, 2)
+
+
+    @total_distance.setter
+    def total_distance(self, total_distance):
+        if self.__total_distance == 0 and self.__total_distance == total_distance:
+            raise Exception
+
+    def new_workout(self, total_distance, total_time):
+        #incrementa total_distance
+        self.__total_distance += total_distance
+        self.__total_time += total_time
+
+# Runner class
 class Runner(Athlete): # Hereda de Athlete Class
 
-    def __init__(self, distance=0, time=0):
+    def __init__(self, distance=0, time=0, speed=0):
         super().__init__(distance, time) # Hereda solo variable instancia
-        #self.__speed = speed
+
+    def run(self):                           #llamar variable de instancia
+        return "I'm a Runner and Ran " + str(self.total_distance) + " meters, time: " + str(self.total_time) + " seconds, speed: " + str(self.speed_record()) + " m/s"
+
+# Swimmer class
+class Swimmer(Athlete):
+
+    def __init__(self, distance=0, time=0, speed=0):
+        super().__init__(distance, time) # Hereda solo variable instancia
+
+    def swim(self):                           #llamar variable de instancia
+        return "I'm a Swimmer and Swim " + str(self.total_distance) + " meters, time: " + str(self.total_time) + " seconds, speed: " + str(self.speed_record()) + " m/s"
+                #"Swam"  meters, time: 10 seconds, speed: 5.0 m/s"
+
+#Cyclist class
+class Cyclist(Athlete):
+
+    def __init__(self, distance=0, time=0, speed=0):
+        super().__init__(distance, time)
+
+    def roll_a_bicla(self):                           #llamar variable de instancia
+        return "I'm a Cyclist and Roll " + str(self.total_distance) + " meters, time: " + str(self.total_time) + " seconds, speed: " + str(self.speed_record()) + " m/s"
 
 
-    #
-    # def total_time(self, total_time):
-    #     self.__total_time =
+# Pruebas a mano de Cyclist lass
+cyclist_1 = Cyclist(75, 5)
+print(cyclist_1.roll_a_bicla() == "I'm a Cyclist and Roll 75 meters, time: 5 seconds, speed: 15.0 m/s")
+cyclist_2 = Cyclist(85, 7)
+print(cyclist_2.roll_a_bicla() == "I'm a Cyclist and Roll 85 meters, time: 7 seconds, speed: 12.14 m/s")
 
+# Pruebas a mano de Swimmer class
+# swimmer_1 = Swimmer(50, 5)
+# print(swimmer_1.swim() == "I'm a Swimmer and Swim 50 meters, time: 5 seconds, speed: 10.0 m/s")
+# swimmer_2 = Swimmer(70, 12)
+# print(swimmer_2.swim() == "I'm a Swimmer and Swim 70 meters, time: 12 seconds, speed: 5.83 m/s")
+# """Ejemplo de entrada y salida"""
 
-
-
-
-
-
-
-
-
-
-
-
-
-"""Ejemplo de entrada y salida"""
-
+# runner_1 = Runner(0, 0)
+# print(runner_1.run())
+# runner_2 = Runner()
+# print(runner_2.run())
 # #instancia de atleta con distancia en metros y tiempo en segundos
 # swimmer = Swimmer(50, 10)
 #
--- athlete/test_athlete.py
+++ athlete/test_athlete.py
@@ -51,63 +51,79 @@ class RunnerTests(unittest.TestCase):
 			raised = True
 		self.assertFalse(raised, "Exception Raised")
 
-# 	def test_total_time_total_time_as_getter(self):
-# 		raised = False
-# 		try:
-# 			self.runner_1.total_time
-# 		except:
-# 			raised = True
-# 		self.assertFalse(raised, "Exception Raised")
-#
-# 	def test_total_time_total_time_as_setter(self):
-# 		raised = False
-# 		try:
-# 			self.runner_1.total_time = 20
-# 		except:
-# 			raised = True
-# 		self.assertFalse(raised, "Exception Raised")
+	def test_total_time_total_time_as_getter(self):
+		raised = False
+		try:
+			self.runner_1.total_time
+		except:
+			raised = True
+		self.assertFalse(raised, "Exception Raised")
+	#2da parte
+	def test_total_time_total_time_as_setter(self):
+		raised = False
+		try:
+			self.runner_1.total_time = 20
+		except:
+			raised = True
+		self.assertFalse(raised, "Exception Raised")
 
-# 	def test_speed_record_if_speed_record_exists(self):
-# 		raised = False
-# 		try:
-# 			self.runner_1.speed_record()
-# 		except:
-# 			raised = True
-# 		self.assertFalse(raised, "Exception Raised")
-#
-# 	def test_total_distance_total_distance_as_getter(self):
-# 		raised = False
-# 		try:
-# 			self.runner_1.total_distance
-# 		except:
-# 			raised = True
-# 		self.assertFalse(raised, "Exception Raised")
-#
-# 	def test_total_distance_total_distance_as_setter(self):
-# 		raised = False
-# 		try:
-# 			self.runner_1.total_distance = 10
-# 		except:
-# 			raised = True
-# 		self.assertFalse(raised, "Exception Raised")
-#
-# 	def test_run_returns_a_string_with_total_distance_total_time_and_speed_when_distance_0_and_time_0(self):
-# 		self.assertEqual(self.runner_1.run(), "I'm a Runner and Ran 0 meters, time: 0 seconds, speed: 0 m/s")
-#
-# 	def test_run_returns_a_string_with_total_distance_total_time_and_speed_when_distance_greater_than_0_and_time_greater_than_0(self):
-# 		self.assertEqual(self.runner_2.run(), "I'm a Runner and Ran 10 meters, time: 10 seconds, speed: 1.0 m/s")
-#
-# 	def test_new_workout_if_new_workout_increments_total_distance_and_total_time(self):
-# 		raised = False
-# 		try:
-# 			self.runner_1.new_workout(20, 7)
-# 		except:
-# 			raised = True
-# 		self.assertFalse(raised, "Exception Raised")
-# 		self.assertEqual(self.runner_1.run(), "I'm a Runner and Ran 20 meters, time: 7 seconds, speed: 2.86 m/s")
-#
-# class SwimmerTests(unittest.TestCase):
-# 	pass
+	def test_speed_record_if_speed_record_exists(self):
+		raised = False
+		try:
+			self.runner_1.speed_record()
+		except:
+			raised = True
+		self.assertFalse(raised, "Exception Raised")
+
+	def test_total_distance_total_distance_as_getter(self):
+		raised = False
+		try:
+			self.runner_1.total_distance
+		except:
+			raised = True
+		self.assertFalse(raised, "Exception Raised")
+
+	def test_total_distance_total_distance_as_setter(self):
+		raised = False
+		try:
+			self.runner_1.total_distance = 10
+		except:
+			raised = True
+		self.assertFalse(raised, "Exception Raised")
+
+	def test_run_returns_a_string_with_total_distance_total_time_and_speed_when_distance_0_and_time_0(self):
+		self.assertEqual(self.runner_1.run(), "I'm a Runner and Ran 0 meters, time: 0 seconds, speed: 0 m/s")
+
+	def test_run_returns_a_string_with_total_distance_total_time_and_speed_when_distance_greater_than_0_and_time_greater_than_0(self):
+		self.assertEqual(self.runner_2.run(), "I'm a Runner and Ran 10 meters, time: 10 seconds, speed: 1.0 m/s")
+
+	def test_new_workout_if_new_workout_increments_total_distance_and_total_time(self):
+		raised = False
+		try:
+			self.runner_1.new_workout(20, 7)
+		except:
+			raised = True
+		self.assertFalse(raised, "Exception Raised")
+		self.assertEqual(self.runner_1.run(), "I'm a Runner and Ran 20 meters, time: 7 seconds, speed: 2.86 m/s")
+
+class SwimmerTests(unittest.TestCase):
+
+	def setUp(self):
+		self.swimmer_1 = Swimmer(50, 5)
+
+	def test_swim_returns_a_string_with_total_distance_total_time_and_speed_when_distance_50_and_time_5(self):
+		self.assertEqual(self.swimmer_1.swim(), "I'm a Swimmer and Swim 50 meters, time: 5 seconds, speed: 10.0 m/s")
+
+#crear Cyclist class *************
+
+class CyclistTest(unittest.TestCase):
+
+	def setUp(self):
+		self.cyclist_1 = Cyclist(75, 5)
+
+	def test_roll_a_bicla_returns_a_string_with_total_distance_total_time_and_speed_when_distance_75_and_time_5(self):
+		self.assertEqual(self.cyclist_1.roll_a_bicla(), "I'm a Cyclist and Roll 75 meters, time: 5 seconds, speed: 15.0 m/s")
+
 
 if __name__=="__main__":
     unittest.main()
